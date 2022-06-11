208433ceb3048c8e782f520ab73e5ab80c34751b
haresh narola <hareshnarola101@gmail.com>
2022-06-11 18:57:02 +0530

    getEventsWithWorkshops

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Date;
+use App\Models\Workshop;
 
 class EventsController extends BaseController
 {
@@ -101,7 +102,10 @@ class EventsController extends BaseController
      */
 
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        $getEventsWithWorkshops = Event::with('workshops')->get();
+
+        return response()->json($getEventsWithWorkshops, 200);
+        // throw new \Exception('implement in coding task 1');
     }
 
 
